bcfec68187d60e05d48b0c5b274286df23e618f0
vprusa <prusa.vojtech@gmail.com>
2023-01-15 22:12:27 +0100

    bkp

--- 3.py
+++ 3.py
@@ -1,26 +1,13 @@
-import numpy as np
-from functools import reduce
-import os
-
-# from keras.utils.generic_utils import default
-
-# import distributed
-# from opts import Parser, Option, BooleanOption
-
 '''
 https://homel.vsb.cz/~kro080/PAI-2022/U3/ukol3.html
 
 https://youtrack.jetbrains.com/issue/PY-52273/Debugger-multiprocessing-hangs-pycharm-2021.3
 https://youtrack.jetbrains.com/issue/PY-37366/Debugging-with-multiprocessing-managers-no-longer-working-after-update-to-2019.2-was-fine-in-2019.1#focus=Comments-27-4690501.0-0
 '''
+import os
 import itertools as it
 import multiprocessing as mp
-import networkx as nx
-from pprint import pprint
-import random
 import matplotlib.pyplot as plt
-import numpy as np
-# from base import *
 import ast
 
 try:
@@ -28,13 +15,12 @@ try:
 except UserWarning:
     pass
 import matplotlib.animation
-import random
 
 matplotlib.use("TkAgg")
 
 import networkx as nx
 from pprint import pprint
-from pprint import pformat
+
 
 def V(G: nx.Graph):
     return G.nodes()
@@ -44,10 +30,6 @@ def E(G: nx.Graph):
 
 def dbg(v, *args):
     if len(args) > 0:
-        # if len(args) == 1:
-        #     pprint(v + ": " + pformat(args[0]))
-        #     pass
-        # else:
         print(v + ": ")
         for i in args:
             pprint(i,  width=200)
@@ -157,14 +139,6 @@ class Vis2D(object):
                 for g in ret:
                     s.G.add_edges_from(g.edges(data=True))
                     s.G.add_nodes_from(g.nodes(data=True))
-                # vypsani nejlepsich reseni
-                # print("Best Value:", best_found.value[0], "postfix permutation: ", best_found.value[1])
-
-                # ulozeni vysledku do souboru
-                # output_file = open('1.py.res.txt', 'w')
-                # output_file.write('Val:\n' + str(best_found.value[0]))
-                # output_file.write('Perm:\n')
-                # kazde vlakno v poolu se stara o cast vypoctu, rozdeleno dle seznamu 'splitter'
 
         return s.G
 
@@ -200,22 +174,6 @@ class Vis2D(object):
                 idx = idx + 1
         s.labels = None
         s.labels_old = None
-        # generates random weights to graph
-        # for (u, v) in s.G.nodes(data=True):
-        #     v['pos'] = (random.randint(s.distances[0], s.distances[1]), random.randint(s.distances[0], s.distances[1]))
-        # for (u, v, w) in s.G.edges(data=True):
-        #     # w['weight'] = (random.randint(1, 40))
-        #     u1 = s.G.nodes()[u]['pos']
-        #     v1 = s.G.nodes()[v]['pos']
-        #     real_dist = np.sqrt(np.power(u1[0]-v1[0], 2) + np.power(u1[1]-v1[1], 2))
-        #     w['weight'] = int(real_dist)
-
-        # or load graph with weights them directly...
-        # s.G = nx.from_edgelist(list(
-        #     [(0, 1, {'weight': 15}), (0, 3, {'weight': 34}), (0, 4, {'weight': 25}), (1, 2, {'weight': 5}),
-        #      (1, 7, {'weight': 23}), (2, 3, {'weight': 33}), (2, 6, {'weight': 29}), (3, 5, {'weight': 13}),
-        #      (4, 5, {'weight': 5}), (4, 7, {'weight': 20}), (5, 6, {'weight': 38}), (6, 7, {'weight': 3})]
-        #     ))
 
         if Vis2D.run_vis:
             s.plt = plt
@@ -330,46 +288,19 @@ class Vis2D(object):
         if (null_nodes is not None):
             null_nodes.set_edgecolor("black")
             s.labels_old = s.labels
-            # s.labels = dict(zip(set(s.G.nodes()), set(list(
             s.labels3 = dict()
             for n in s.G.nodes(data=True):
                 n[1]['label'] = str(n[0]) + "\n" + str(round(n[1]['cur_pr'], 4))
                 s.labels3[n[0]] = n[1]['label']
-            #     map(lambda x: str(x[0]) + "\n" + str(round(x[1]['cur_pr'], 4)), s.G.nodes(data=True))))))
             s.labels = dict(zip(set(s.G.nodes()), set(list(
-                # map(lambda x: str(round(x[1]['cur_pr'], 4)), s.G.nodes(data=True))))))
             map(lambda x: x[1]['label'], s.G.nodes(data=True))))))
-            # s.labels = dict(zip(set(s.G.nodes()), set(list(
-            #     map(lambda x: str(x[0]) + "\n" + str(round(x[1]['cur_pr'], 4)), s.G.nodes(data=True))))))
-            # nx.draw_networkx_labels(s.G, pos=s.layout, labels=s.labels, font_size=6, font_color="red", ax=s.ax)
             s.labels2 = {key: val for key, val in sorted(s.labels.items(), key=lambda ele: ele[0], reverse=False)}
         nx.draw_networkx_labels(s.G, pos=nx.circular_layout(s.G), labels=s.labels3, font_size=6, font_color="red", ax=s.ax)
-        # Query nodes
-        # s.idx_colors = sns.cubehelix_palette(len(forestNodes), start=.5, rot=-.75)[::-1]
-        color_map = []
-
-        # query_nodes = nx.draw_networkx_nodes(s.G, pos=s.layout, nodelist=forestNodes,
-        #                                      node_color=s.idx_colors[:len(forestNodes)], ax=s.ax)
-
-        # if query_nodes is not None:
-        #     query_nodes.set_edgecolor("white")
-        # nx.draw_networkx_labels(s.G, pos=s.layout, labels=dict(zip(forestNodes[0], forestNodes[0])), font_color="red", ax=s.ax)
-        # nx.draw_networkx_labels(s.G, pos=s.layout, labels=dict(zip(forestNodes, forestNodes)), font_color="white", ax=s.ax)
 
         edges = list((l[0], l[1]) for l in edges)
         dbg("edges", edges)
-        # nx.draw_networkx_edges(s.G, pos=s.layout, edgelist=edges, width=s.idx_weights[:len(edges)] ) # , ax=s.ax, arrowstyle='->', arrowsize=10
-
         # draw weights
-        labels = nx.get_edge_attributes(s.G, 'weight')
-        # nx.draw_networkx_edge_labels(s.G, s.layout, edge_labels=labels)
-
-        # Scale plot ax
-        # s.ax.set_xticks([])
-        # s.ax.set_yticks([])
-
         s.ax.set_title("Step #{}, Price: {}".format(Vis2D.frameNo, s.sum_prices()))
-        # s.show_axes()
 
         s.plt.pause(s.frameTimeout)
         Vis2D.frameNo += 1
@@ -378,17 +309,13 @@ class Vis2D(object):
         s.ax.set_xlim(s.distances[0]-(s.distances[1]*0.1), s.distances[1]+(s.distances[1]*0.1))
         s.ax.set_ylim(s.distances[0]-(s.distances[1]*0.1), s.distances[1]+(s.distances[1]*0.1))
         s.ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
-  # def load_graph_part(s, part_file_path, load_threads_cnt, part_i, lock):
 
-# def load_graph_part(G2, whole_file_path, part_i, lock):
 def load_graph_part(whole_file_path, part_i):
     '''
     The parallelization here is in reading files to SUB-Graph of whole graph.
     The subgraph is added to in-memory Graph representation.
     So the effectiveness is in loading files from disk.
     '''
-    # vypsani nejlepsich reseni
-    # for part_i in range(0, load_threads_cnt):
     file_path = whole_file_path + "." + str(part_i)
     G = nx.DiGraph()
     f = open(file_path)
@@ -408,7 +335,6 @@ def load_graph_part(whole_file_path, part_i):
             if count % Vis2D.debug_loading_divider == 0:
                 print("Part: {}, line {}: {}".format(part_i, count, line.strip()))
 
-        # print("Line{}: {}".format(count, line.strip()))
     f.close()
     return G
 
@@ -417,54 +343,22 @@ class PageRank(Vis2D):
     pass
 
 # sample data
-# file_path = "web-BerkStan.cube.txt"
+file_path = "web-BerkStan.cube.txt"
 # file_path = "web-BerkStan.one-way-cube.txt"
 # file_path = "web-BerkStan.pentagram-full.txt"
 # file_path = "web-BerkStan.pentagram-one-way.txt"
 # file_path = "web-BerkStan.pentagram-one-way-reverse.txt"
 # file_path = "web-BerkStan.pentagram-one-way-with-one-sink.txt"
 # file_path = "web-BerkStan.pentagram-one-way-with-one-sink.txt"
-file_path = "web-BerkStan.txt"
+# file_path = "web-BerkStan.txt"
 # file_path = "web-BerkStan.head_200.txt"
 # Vis2D.run_vis = True
 Vis2D.run_vis = False
 
-# graph_data_str = "[("+"1   2\n2   3\n1   4".replace("   ", ",").replace("\n","),(")+")]"
-# graph_data_str = "[("+text3.replace("	", ",").replace("\n","),(")+")]"
-
-
-# # run_vis = True
-# run_vis = False
-# debug_loading = True
-# debug_loading_divider = 10000
-# debug_eval = True
-# show_n_best = 3
-# load_threads_cnt = 4
-# # skip_splitting = True
-# skip_splitting = False
-
-
-# parser = Parser(options={
-#     "file_path": Option("f", "--file", short_description=u"file_path"),
-#     "debug_loading_divider": Option("l", "--debug_loading_divider", short_description=u"debug_loading_divider"),
-#     "debug_eval": Option("e", "--debug_eval", short_description=u"debug_eval"),
-#     "load_threads_cnt": Option("t", "--load_threads_cnt", short_description=u"load_threads_cnt"),
-#     "show_n_best": Option("b", "--show_n_best", short_description=u"show_n_best"),
-#
-#     "run_vis": BooleanOption("v", "run_vis", default=False, short_description=u"run_vis"),
-#     "debug_loading": BooleanOption("l", "debug_loading", default=False, short_description=u"debug_loading"),
-#     "skip_splitting": BooleanOption("s", "skip_splitting", default=False, short_description=u"skip_splitting"),
-# })
-
-# options, arguments = parser.evaluate()
 import argparse
-# from argparse import ArgumentParser as parser
-
 parser = argparse.ArgumentParser(prog='PROG', allow_abbrev=False)
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',help='')
 parser.add_argument('-f', '--file_path', default=file_path, help='file_path')
 
-# parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='')
 parser.add_argument('-v','--run_vis', default=False, help='default=max, ')
 args = parser.parse_args()
 
@@ -472,7 +366,5 @@ pprint(args)
 Vis2D.run_vis = args.run_vis
 file_path = args.file_path
 
-# r = TSP(graphData=[(1, 2), (1, 3)])
 r = PageRank(file_path=file_path)
-# r = TSP()
 r.alg()
